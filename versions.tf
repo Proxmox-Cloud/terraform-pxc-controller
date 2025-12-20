197941daacf5ad9257558380911c3820a92d9ee8
tobi <tobi_h@outlook.com>
2025-12-20 19:36:07 +0000

    Update pxc provider version to 0.0.20

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     pxc = {
       source = "pxc/proxmox-cloud"
-      version = ">~ 0.0.1" # pxc sed ci - DONT REMOVE COMMENT!
+      version = ">~ 0.0.20" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
