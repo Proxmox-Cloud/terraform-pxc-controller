930cf3d252713410146bd970a8722166ee1cd793
tobi <tobi@tobias-huebner.tech>
2026-01-04 08:33:40 +0000

    Update pxc provider version to 0.0.42

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>0.0.41" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>0.0.42" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
