f56a386f8dcaf4d8a3c6740e8ee604cda843bcc6
tobi <tobi@tobias-huebner.tech>
2026-01-23 14:09:20 +0000

    Set pxc provider rc version to 0.2.0-rc3

--- tests/scenarios/deployments/versions.tf
+++ tests/scenarios/deployments/versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "0.2.0-rc2" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "0.2.0-rc3" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
