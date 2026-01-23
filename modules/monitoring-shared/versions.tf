a07d04a934ce58ad1bcae5b254fe786fd7cb17db
tobi <tobi@tobias-huebner.tech>
2026-01-23 11:22:45 +0000

    Set pxc provider rc version to 0.2.0-rc1

--- modules/monitoring-shared/versions.tf
+++ modules/monitoring-shared/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>0.1.6" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "0.2.0-rc1" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
