51ea1b4cc796731366dea1afcdeb36b622c0df6f
tobi <tobi@tobias-huebner.tech>
2026-02-19 11:04:16 +0000

    Update pxc provider version to 0.3.0

--- modules/harbor-mirror-projects/versions.tf
+++ modules/harbor-mirror-projects/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>0.2.8" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>0.3.0" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
