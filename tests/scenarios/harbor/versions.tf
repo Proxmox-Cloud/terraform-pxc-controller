7a77d633d60c357da0904fe3f89928408a6997b7
tobi <tobi@tobias-huebner.tech>
2026-02-27 12:28:45 +0000

    Update pxc provider version to 3.0.2

--- tests/scenarios/harbor/versions.tf
+++ tests/scenarios/harbor/versions.tf
@@ -16,7 +16,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>3.0.1" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>3.0.2" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
