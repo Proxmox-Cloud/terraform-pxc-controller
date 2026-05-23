198e028b13e1ee9cbb421550f6d2c455809dbf64
tobi <tobi@tobias-huebner.tech>
2026-05-23 10:55:44 +0000

    Update pxc provider version to 3.1.4

--- modules/harbor-cluster-robot/versions.tf
+++ modules/harbor-cluster-robot/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>3.1.3" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>3.1.4" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
