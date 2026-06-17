89936aa6398db1e91e718812c6c80fed4e69e4e7
tobi <tobi_h@outlook.com>
2026-06-17 11:57:30 +0000

    fix missing e2e var

--- modules/harbor-mirror-projects/variables.tf
+++ modules/harbor-mirror-projects/variables.tf
@@ -1,3 +1,9 @@
 variable "harbor_host" {
   type = string
 }
+
+variable "skip_pxc_secret_creation_e2e" {
+  type = bool
+  default = false
+  description = "Can be toggeled on to rely on externally injected discovery of remote artifactory for faster e2e testing."
+}
