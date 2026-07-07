be0c5d74e9f29dce96186f193dbba7f87feb0341
tobi <tobi@tobias-huebner.tech>
2026-07-07 08:10:44 +0000

    Update pxc provider version to 3.3.4

--- modules/monitoring-master-stack/versions.tf
+++ modules/monitoring-master-stack/versions.tf
@@ -26,7 +26,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>3.3.3" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>3.3.4" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
