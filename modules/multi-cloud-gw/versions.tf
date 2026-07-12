b9df505872c35f05d19f9e09a0e900f113ed7e4c
tobi <tobi@tobias-huebner.tech>
2026-07-12 11:52:19 +0000

    Update pxc provider version to 3.3.7

--- modules/multi-cloud-gw/versions.tf
+++ modules/multi-cloud-gw/versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>3.3.6" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>3.3.7" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
