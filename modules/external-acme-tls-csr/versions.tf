e868b245809a50df13cf3f217d5e48fad9fbf35b
tobi <tobi@tobias-huebner.tech>
2026-07-27 21:58:02 +0000

    Update pxc provider version to 3.4.1

--- modules/external-acme-tls-csr/versions.tf
+++ modules/external-acme-tls-csr/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>3.4.0" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>3.4.1" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
