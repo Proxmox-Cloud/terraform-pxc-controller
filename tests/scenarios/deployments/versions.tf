031ba7cccb2dbd0241004beaa2affd22909f1882
tobi <tobi@tobias-huebner.tech>
2026-08-11 17:34:06 +0000

    Update pxc provider version to 3.5.0

--- tests/scenarios/deployments/versions.tf
+++ tests/scenarios/deployments/versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>3.4.1" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>3.5.0" # pxc sed ci - DONT REMOVE COMMENT!
     }
     dns = {
       source = "hashicorp/dns"
