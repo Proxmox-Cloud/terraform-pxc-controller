b826e87cc63462ba17b487f0c664072570a56b3e
tobi <tobi@tobias-huebner.tech>
2026-08-12 10:18:59 +0000

    Update pxc provider version to 3.5.1

--- modules/monitoring-shared/versions.tf
+++ modules/monitoring-shared/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>3.5.0" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>3.5.1" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
--- modules/multi-cloud-gw/versions.tf
+++ modules/multi-cloud-gw/versions.tf
@@ -10,7 +10,7 @@ terraform {
     }
     pxc = {
       source = "Proxmox-Cloud/pxc"
-      version = "~>3.5.0" # pxc sed ci - DONT REMOVE COMMENT!
+      version = "~>3.5.1" # pxc sed ci - DONT REMOVE COMMENT!
     }
   }
 }
